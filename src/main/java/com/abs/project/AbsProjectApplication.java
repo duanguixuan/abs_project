78c2103bb5ce1e06d0ec3e1383bee408256cb1e0
duanguixuan <l>
2019-10-16 11:32:50 +0800

    项目初始化

---
+++ src/main/java/com/abs/project/AbsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.abs.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsProjectApplication.class, args);
+	}
+
+}
